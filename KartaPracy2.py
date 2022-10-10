5796bf0e2b2212033869b57bc49d317e61738c31
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-10-10 10:20:09 +0000

    Dodano zadania 1-4

---
+++ KartaPracy2.py
@@ -0,0 +1,27 @@
+# # Zad 1
+# n = int(input())
+# if n % 3 == 0:
+#   print ("tak")
+# else:
+#   print("Nie") 
+
+# # Zad 2
+# n = int(input())
+# if n >= 100 and n<1000 and n%17 == 0 :
+#   print ("tak")
+# else: 
+#   print("nie")
+
+# Zad 3
+# wiek = int(input())
+# if wiek >= 18:
+#   print ("tak") 
+# else: 
+#   print ("nie")
+
+# Zad 4 
+# waga = int(input())
+# if waga <=LIMIT: 
+#   print ("tak ")
+# else:
+#   print ("nie ")
