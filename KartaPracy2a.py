9485f2dd3e8531b00c3695de3c04b2b8943b5175
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-10-12 17:42:48 +0000

    KartaPracy2a 1-3 zrobione

---
+++ KartaPracy2a.py
@@ -0,0 +1,35 @@
+# # Zad 1 
+# a = int(input())
+# b = int(input())
+# if ((a + b ) % 2) ==0 :
+#   print ("parzysta")
+# else:
+#   print ("nieparzysta")
+
+# Zad 2
+# a = int(input())
+# g = int(input())
+# if (a+g) / 2 > (a * g) **(1/2):
+#   print ("tak")
+# else:
+#   print("nie")
+
+# Zad 2
+# import math
+# a = int(input())
+# g = int(input())
+# if a+g % 2 > math.sqrt(a*g):
+#    print ("tak")
+# else:
+#   print("nie")
+
+# Zad 3
+# k, l, m = int(input()), int(input()), int(input())
+# if k==l :
+#    print ("tak k +l")
+# elif  l==m :
+#   print("tak l+m ")
+# elif k==m:
+#   print("tak k+m")
+
+# Zad 4 
