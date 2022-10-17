1e40a3969292c42574c0361125d2de81826456e5
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-10-17 10:20:02 +0000

    cwiczenia

---
+++ KartaPracy3.py
@@ -0,0 +1,27 @@
+# PRE
+# print(list(range(2, 8,1 )))
+# print(list(range(1, 8,)))
+# print(list(range(8, 1 , -1 )))
+
+# pętla liczb dwucyfrowych od 10 do 21
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+
+# for i in range(3,10):
+#   print (i)
+
+# for n in range(10, 22):
+#   print (n)
+
+# for n in range (15, 32, 2): 
+#   print (n)
+
+# for n in range (100, 0, -1 ):
+#   print (n)
+  
+# for 
+
+
