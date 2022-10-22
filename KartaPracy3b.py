42d8494b11ec3884509a16dfaf3cfb2086368d06
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-10-22 13:00:53 +0000

    zrobione zadania 1-5

---
+++ KartaPracy3b.py
@@ -0,0 +1,46 @@
+# Zad 1 
+# for dni in range (1, 31):
+  
+#   print (dni, end = " ")
+
+# Zad 2 
+# for n in range(1, 100, 2):
+#   print (n**2, end = " " )
+
+# Zad 3 
+# for n in range (1000, 10000):
+#   if n % 379 == 0:
+#     print (n)
+
+# # Zad 4 
+# for n in range (100, 1000):
+#   if n % 5 == 0 and n % 6 == 0 and n % 11 == 0:
+#     print (n )
+
+# Zad 5
+#suma = 0
+#n = int(input())
+#nliczb = int(input())
+#for n in range (n):
+  #print (n + suma)
+
+# n=int(input())
+# suma=0
+# for i in range(n):
+#   a=int(input())
+#   suma = suma + a
+# print (suma)
+
+  
+
+
+
+
+
+  #Zad 10
+import math 
+for n in range(1, 1001):
+  if math.sqrt(n) == n:
+     print(n)
+  else:
+     n+=1
