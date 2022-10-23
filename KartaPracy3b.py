09d9c3f70d6ff3cc0c6837e9f6f2c0e559b51f3b
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-10-23 15:03:14 +0000

    zrobione zada nia 5-10 bez 8

--- KartaPracy3b.py
+++ KartaPracy3b.py
@@ -1,29 +1,33 @@
 # Zad 1 
+# print("Zadanie 1")
+
 # for dni in range (1, 31):
   
 #   print (dni, end = " ")
 
+# zad 1 
+# from calendar import month
+# print (month(2022,11))
+
 # Zad 2 
+# print("Zadanie 2")
 # for n in range(1, 100, 2):
 #   print (n**2, end = " " )
 
 # Zad 3 
+# print("Zadanie 3")
 # for n in range (1000, 10000):
 #   if n % 379 == 0:
 #     print (n)
 
 # # Zad 4 
+# print("Zadanie 4")
 # for n in range (100, 1000):
 #   if n % 5 == 0 and n % 6 == 0 and n % 11 == 0:
 #     print (n )
 
 # Zad 5
-#suma = 0
-#n = int(input())
-#nliczb = int(input())
-#for n in range (n):
-  #print (n + suma)
-
+# print("Zadanie 5")
 # n=int(input())
 # suma=0
 # for i in range(n):
@@ -31,16 +35,47 @@
 #   suma = suma + a
 # print (suma)
 
-  
+# # Zad 6
+# print("Zadanie 6")
+# n=int(input())
+# suma = 0 
+# for n in range (2, n*2, 2):
+#   suma += n 
+# print (suma)
+
+# Zad 7 
+# print("Zadanie 7")
+# n = int(input())
+# suma = 0 
+# for n in range(11, (n*2)+11, 2):
+#   suma += n 
+# print (suma)
+
+# Zad 8 
 
 
 
 
 
-  #Zad 10
+
+
+# Zad 9 
+# print("Zadanie 9")
+# n = int(input())
+# j = 21
+# suma = 0 
+# for i in range(0 , n+1):
+#   for i in range(0, n , j ):
+#     print (j)
+#     suma += j 
+#   j += 100
+# print ( suma )
+
+#   #Zad 10
 import math 
-for n in range(1, 1001):
-  if math.sqrt(n) == n:
+print("Zadanie 10")
+for n in range(1, 1000):
+  if i - (i//10)*10 == math.sqrt(n):
      print(n)
-  else:
-     n+=1
+    else i - (i//100)*100 == math.sqrt(n) 
+print(n )
