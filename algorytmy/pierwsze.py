c0b1837ec6e3651a67f0cd8a9055ed5f808b0b38
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-11-21 11:15:58 +0000

    zrobione liczby oierwsze

--- algorytmy/pierwsze.py
+++ algorytmy/pierwsze.py
@@ -1,11 +1,22 @@
-#1 sprawdzanie czy liczba jest 1 
+# #1 sprawdzanie czy liczba jest 1 
+
 n = int(input())
 for i in range (2, n ):
   if n % i == 0 :
     print("lizcba nie jest pierwsza ")
     exit(0)
-print("liczba jest pierwsza")
+print("liczba jest pierwsza")85
 
 
 
 #2. generowanie liczb 1 
+
+n = int(input("do jakiej liczby pierwszej "))
+for i in range(2, n+1 ):
+  flaga = True
+  for j in range(2, i ):
+    if i % j == 0 :
+      flaga = False
+      break
+  if flaga: 
+    print ( i, end = " ")
