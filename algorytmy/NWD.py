37e3bc3c24d4719ded7e6f9e93b888d0377c50c4
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-11-30 13:14:57 +0000

    NWW

--- algorytmy/NWD.py
+++ algorytmy/NWD.py
@@ -8,8 +8,30 @@
 
 
 # euklides modulo
+# a = int(input())
+# b = int(input())
+# while b>0:
+#   a, b = b, a%b 
+#   print (a)
+
+
+
+# # NWW - odej omwanie
+# a = int(input())
+# b = int(input())
+# c = a * b 
+# while a != b :
+#     if a > b : a = a - b 
+#     if b > a : b = b - a 
+#     print ("NWD ", a) 
+#     break
+# d = c / a  
+# print ("NWW", d)\
+
+
 a = int(input())
 b = int(input())
 while b>0:
   a, b = b, a%b 
-  print (a)
+NWD = a 
+print(a)
