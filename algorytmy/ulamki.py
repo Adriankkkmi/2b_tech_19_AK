702d04f9c6c4ca16a8ffd49d9a7d0e7bebedc14e
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2022-12-07 13:10:54 +0000

    zrobiono ulamki

---
+++ algorytmy/ulamki.py
@@ -0,0 +1,14 @@
+a = int(input("gora pierwszego ulamka "))
+b = int(input("dol pierwszego ulamka "))
+c = int(input("gora drugiego ulamka "))
+d = int(input("dul drugiego ulamka "))
+x, y = b, d 
+iloczyn = x * y
+while y > 0:
+    x, y = y, x % y
+nww = iloczyn // x 
+e = (nww // b) * a 
+t = (nww // d) * c 
+g = e + t
+print (f" {a} / {b} +  {c}/{d} = {e}/{nww} + {t}/{nww} = {g}/{nww}")
+
