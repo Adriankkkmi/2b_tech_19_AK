59c5c1922b2c0d70b8a3180b664aaad478e04d98
1B8AK <114583259+Adriankkkmi@users.noreply.github.com>
2023-03-06 11:17:01 +0000

    nap2

---
+++ napisy/nap2.py
@@ -0,0 +1,43 @@
+# palindrom za pomocą tablicy
+
+# s = input()
+
+# for i in range(len(s)//2):
+#     if s[i] != s[len(s)-1-i]:
+#         exit("NIE JEST")
+# exit("JEST PALINDROMEM")
+
+# anagram za pomocą tablicy
+# dama i adam
+# a =input()
+# b = input()
+# A = list(a)
+# B = list(b)
+# A.sort()
+# B.sort()
+
+# a = " ".join(A)
+# b = " ".join(B)
+# print(a,b)
+# if a==b:
+#   print("tak")
+# else:
+#   print("nie")
+
+########
+
+
+
+
+
+
+
+
+
+
+
+
+#L = [5, 7, 1, 4]
+# print(s, L)
+# L.sort()
+# print(s, L)
