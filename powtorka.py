dab1eb59e9387dfcac094caeb397a3df5a42a98f
Adriankkkmi <114583259+Adriankkkmi@users.noreply.github.com>
2023-09-20 12:13:17 +0000

    karty pracy robione defem

---
+++ powtorka.py
@@ -0,0 +1,53 @@
+#zad1
+# def oblicz(a,b):
+#   return a**2 + b**2
+
+# print(oblicz(3,4))
+
+#zad2 
+# def oblicz (a,b):
+#   return (a+b)**2
+# print oblicz (2,2)
+
+#zad3
+
+# def oblicz(a,b):
+#   return (a-b)**3
+# print oblicz (2,3)
+
+#zad4
+# def oblicz(a,b,c):
+#   return a *b *c
+# print oblicz ()
+
+# #zad5
+# def oblicz(a,b):
+#   return 2 * (a +b) /5 
+# print oblicz ()
+  
+# #zad6 
+# def oblcz (a):
+#   return a /1,23
+
+# print oblicz()
+
+# #zad7
+# def oblicz(a,b):
+#   return a%b
+
+# print oblicz ()
+
+
+#karta pracy 2
+#zad1
+# a = int(input())
+# def jezeli(a):
+#   return a%3 ==0 
+  
+# print jezeli (a)
+
+#zad2
+a = int(input())
+def yo(a):
+  return 
+
